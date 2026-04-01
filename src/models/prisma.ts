01b693fdfc52b0589c84341b8c6fbee37abe0c31
ssk7594-CRz <ssk7594@gmail.com>
2026-04-01 09:21:39 +0900

    fix: 서버 실행 및 DB 연결 설정

--- src/models/prisma.ts
+++ src/models/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "../generated/prisma/client";
 
 const prisma = new PrismaClient({
-  accelerateUrl: process.env.DATABASE_URL ?? "",
+  accelerateUrl: process.env.DATABASE_URL ?? "postgresql://postgres:postgres@localhost:5433/welive",
 });
 
 export default prisma;
